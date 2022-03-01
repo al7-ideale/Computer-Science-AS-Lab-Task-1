5ec4e0c90508c1d940dbc8201459b2ca53555964
Aadarsh Dhami <aadarshdhami15@gmail.com>
2022-03-01 18:09:42 +0545

    Completed

---
+++ math4.py
@@ -0,0 +1,10 @@
+#To Determine the number is odd, even or neutral
+n = int(input("Enter a number: "))
+if n == 0:
+    print(str(n) + " is neutral")
+
+elif n%2 == 0:
+    print(str(n) + " is even")
+
+else:
+    print(str(n) + " is odd")
