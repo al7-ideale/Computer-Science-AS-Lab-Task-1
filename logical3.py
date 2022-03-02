f591dadcc37f99455547c7c009b076b8a7198a68
Aadarsh Dhami <aadarshdhami15@gmail.com>
2022-03-02 16:58:14 +0545

    Logical

---
+++ logical3.py
@@ -0,0 +1,18 @@
+#To Determine whether the student passed or failed the test of two subjects only.
+
+print("Enter the marks in Mathematics: ")
+m = int(input())
+print("Enter the marks in Physics: ")
+p = int(input())
+
+if m >= 40 and p >= 40:
+    print("The student passed in both Mathematics and Physics.")
+    
+elif m >= 40 and p < 40:
+    print("The student passed in Mathematics but failed in Physics.")
+
+elif m < 40 and p >= 40:
+    print("The student passed in Physics but failed in Mathematics.")
+else:
+    print("The student failed in both Mathematics and Physics.")
+    
