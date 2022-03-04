0f811f68a4172f620adc7c6a341d989f3b03f0b5
Aadarsh Dhami <aadarshdhami15@gmail.com>
2022-03-04 13:40:21 +0545

    Comparison

---
+++ Comparison1.py
@@ -0,0 +1,9 @@
+#To Display he larger number among the numbers (at least 2) entered by the user and by how much.
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+if x > y:
+    print(x,"is greater than",y,"by",x-y)
+else:
+    print(y,"is greater than ",x,"by",y-x)
