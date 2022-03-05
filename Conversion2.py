6e4367fa4f6d28498bb471ee6bbaea713c5571a0
Aadarsh Dhami <aadarshdhami15@gmail.com>
2022-03-05 10:57:18 +0545

    Concat

---
+++ Conversion2.py
@@ -0,0 +1,8 @@
+#To join the first name, middle name and last name of a user 
+#which are asked o user to input separately.
+
+first_name = str(input("Enter your first name: "))
+middle_name = str(input("Enter your middle name: "))
+last_name = str(input("Enter your last name: "))
+
+print(first_name,middle_name,last_name)
